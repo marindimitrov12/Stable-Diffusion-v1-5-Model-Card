ea05cc567b6ad444b70859e0edc90addc3036858
marindimitrov12 <63950527+marindimitrov12@users.noreply.github.com>
2023-12-17 18:42:21 +0200

    write text to emage generator

--- textToImageGenerator/Program.py
+++ textToImageGenerator/Program.py
@@ -3,7 +3,20 @@ from diffusers import DiffusionPipeline
 pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", use_safetensors=True)
 
 pipeline
-prompt = "A photograph of an astronaut riding a horse on Mars, high resolution, high definition."
-image = pipeline(prompt, num_inference_steps=10).images[0]
-image
-image.save("image_of_squirrel_painting.png")
+i=0
+items=[]
+while i<3:
+    item = input("Enter prompt:")
+    items.append(item)
+    i+=1
+
+print(items)
+for item in items:
+    prompt = "Cartoon {}.".format(item)
+    print(prompt)
+    image = pipeline(prompt, num_inference_steps=15).images[0]
+
+    image.save("image_{}.png".format(item))
+
+
+print("generation completed!")
